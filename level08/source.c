44fc444e0171c36a5a208031698b3c8528598e24
mpagani <mathiapagani@gmail.com>
2024-04-30 11:49:41 +0200

    adding details and fixed source

--- level08/source.c
+++ level08/source.c
@@ -1,54 +1,70 @@
-void log_wrapper(FILE *fs, char *string, char *filename)
-{
-  char    *content;
-  size_t  idx;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-  strcpy(content, string);
-  snprintf(content, strlen(filename), filename);
-  idx = strcspn(content, "\n");
-  content[idx] = '\0';
-  fprintf(fs, "LOG: %s\n", content);
+void log_wrapper(FILE *log_file, char *message, char *filename) {
+  char buffer[264];
+
+  strcpy(buffer, message);
+  snprintf(buffer + strlen(buffer), 255 - strlen(buffer) - 1, filename);
+  buffer[strcspn(buffer, "\n")] = '\0';
+  fprintf(log_file, "LOG: %s\n", buffer);
 }
 
-int main(int argc, char **argv)
-{
-  FILE  *logFS;
-  FILE  *fileFS;
-  char  *file = "./backups/";
-  int   fd;
-  char  c;
+int main(int argc, char **argv) {
+  char byte;
+  char character;
+  char buffer[100];
+  FILE *source_file,
+  FILE *log_file;
+  int fd = -1;
 
-  if (argc != 2)
-    printf("Usage: %s filename\n",*argv,*argv);
+  if (argc != 2) {
+    printf("Usage: %s filename\n", argv[0]);
+  }
 
-  logFS = fopen("./backups/.log","w");
-  if (logFS == NULL)
-  {
+  log_file = fopen("./backups/.log", "w");
+
+  if (!log_file) {
     printf("ERROR: Failed to open %s\n", "./backups/.log");
     exit(1);
   }
 
-  log_wrapper(logFS, "Starting back up: ", argv[1]);
+  log_wrapper(log_file, "Starting back up: ", argv[1]);
+  source_file = fopen(argv[1], "r");
 
-  fileFS = fopen(argv[1],"r");
-  if (fileFS == NULL)
-  {
+  if (!source_file) {
     printf("ERROR: Failed to open %s\n", argv[1]);
     exit(1);
   }
 
-  strncat(file, argv[1], strlen(argv[1]));
-  fd = open(file, 0xc1, 0x1b0);
+  strcpy(buffer, "./backups/");
+  strncat(buffer, argv[1], 100 - strlen(buffer) - 1);
+  fd = open(buffer, 193);
+
   if (fd < 0) {
     printf("ERROR: Failed to open %s%s\n", "./backups/", argv[1]);
     exit(1);
   }
-  while(c = fgetc(fileFS))
-    write(fd, &c, 1);
 
-  log_wrapper(logFS, "Finished back up ", argv[1]);
+  while(true) {
+    byte = fgetc(source_file)
+    character = (char)byte;
 
-  fclose(fileFS);
+    if (character == -1) {
+      break;
+    }
+    
+    write(fd, &character, 1);
+  }
+
+  log_wrapper(log_file, "Finished back up ", argv[1]);
+  fclose(source_file);
   close(fd);
+
   return 0;
 }
